936f7ff3fe71b3c5ba16d2c7b46414731148cc37
Pradeep Yadav <107470512+prdpbhai@users.noreply.github.com>
2023-04-05 11:36:57 +0530

    Add files via upload

---
+++ jarvis.py
@@ -0,0 +1,110 @@
+import webbrowser
+import pyttsx3
+import speech_recognition as sr
+import datetime
+import wikipedia
+import os
+import smtplib
+
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+engine.setProperty('voice', voices[0].id)
+
+def speak(audio):
+   engine.say(audio)
+   engine.runAndWait()
+   
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
+
+    else:
+        speak("Good Evening!")  
+
+    speak("I am Pradeep Yadav. Please tell me how may I help you")       
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...")  
+        return "None"
+    return query
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('pradeepyadavanm@gmail.com', '161814pv')
+    server.sendmail('pradeepyadavchhotu@gmail.com', to, content)
+    server.close()
+
+if __name__=="__main__":
+   wishMe()
+   while True:
+        query = takeCommand().lower()
+        
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+            
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open whatsapp' in query:
+            webbrowser.open("https://web.whatsapp.com/")
+
+        elif 'open sexy photo' in query:
+            webbrowser.open("https://www.pexels.com/search/sexy%20pic/") 
+            
+        elif 'open google' in query:
+            webbrowser.open("google.com")       
+
+
+        elif 'play music' in query:
+            music_dir = 'D:\\videos\\New folder'
+            songs = os.listdir(music_dir)
+            print(songs)    
+            os.startfile(os.path.join(music_dir, songs[0]))
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+
+        elif 'open sexy video' in query:
+            codePath = "https://www.pexels.com/search/videos/sexy/"
+            os.startfile(codePath)
+  
+
+        elif 'email to pradeep' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "pradeepyadavanm@gmail.com"    
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry my friend harry bhai. I am not able to send this email")        
